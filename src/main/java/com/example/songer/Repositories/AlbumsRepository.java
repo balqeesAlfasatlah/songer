cdfb4a65c2f6a613f9ae14e0546008065dc8327f
balqessAlfasatlah <BalqeesAlfasatlah@yahoo.com>
2021-10-26 14:16:07 +0300

    done

--- src/main/java/com/example/songer/Repositories/AlbumsRepository.java
+++ src/main/java/com/example/songer/Repositories/AlbumsRepository.java
@@ -1,6 +1,8 @@
 package com.example.songer.Repositories;
 
+import com.example.songer.AlbumModel;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AlbumsRepository extends CrudRepository {
+public interface AlbumsRepository extends JpaRepository<AlbumModel,Integer> {
 }
